d5cb134d7b564a1d6076d4ae6f7b33c4014a3cde
HantikMine <vadim_dr@hotmail.com>
2023-11-01 21:42:26 +0300

    -

--- app/config.py
+++ app/config.py
@@ -1,14 +1,13 @@
 from flask import Flask
-
+import os
 app = Flask(__name__)
 
+app.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
 SECRET_KEY = 'ajbsdiuabsidubasdiubasihdasdasdasd'
 MYSQL_USER = 'user'
 MYSQL_PASSWORD = 'password'
 MYSQL_DATABASE = 'std_2191_exam'
 app.config["MYSQL_HOST"] = "${MYSQL_HOST}"
-
-
 
 ADMINISTRATOR_ROLE_ID = 1
 MODERATOR_ROLE_ID = 2
