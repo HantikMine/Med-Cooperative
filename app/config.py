4b9e9e606f95f22f8a71bc40ee448fd961b3155d
HantikMine <vadim_dr@hotmail.com>
2023-11-01 21:44:29 +0300

    -

--- app/config.py
+++ app/config.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'ajbsdiuabsidubasdiubasihdasdasdasd'
 MYSQL_USER = 'user'
 MYSQL_PASSWORD = 'password'
 MYSQL_DATABASE = 'std_2191_exam'
-app.config["MYSQL_HOST"] = "${MYSQL_HOST}"
+
 
 ADMINISTRATOR_ROLE_ID = 1
 MODERATOR_ROLE_ID = 2
