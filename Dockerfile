da7922516660b8465c0c9cc87c969c8a7b875b45
HantikMine <vadim_dr@hotmail.com>
2023-11-01 19:04:44 +0300

    -

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM python:3.9-slim-buster
 
 WORKDIR /app
 
+RUN apt-get update
+RUN apt-get install python3-dev default-libmysqlclient-dev gcc  -y
+
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 
---
+++ app/config.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'ajbsdiuabsidubasdiubasihdasdasdasd'
+
+MYSQL_USER = 'std_2191_exam'
+MYSQL_PASSWORD = '11111111'
+MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
+MYSQL_DATABASE = 'std_2191_exam'
+
+ADMINISTRATOR_ROLE_ID = 1
+MODERATOR_ROLE_ID = 2
