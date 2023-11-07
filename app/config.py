84a00bff3f9e1a445311874e5bfd5d0a91d03fbd
HantikMine <vadim_dr@hotmail.com>
2023-11-07 13:28:15 +0300

    -

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ SECRET_KEY = 'ajbsdiuabsidubasdiubasihdasdasdasd'
 MYSQL_DATABASE_USER = 'user'
 MYSQL_DATABASE_PASSWORD = 'password'
 MYSQL_DATABASE_DB = 'std_2191_exam'
-MYSQL_DATABASE_HOST = os.getenv('MYSQL_HOST', 'db')
+MYSQL_DATABASE_HOST = 'db'
 
 ADMINISTRATOR_ROLE_ID = 1
 MODERATOR_ROLE_ID = 2
