14c39b037759c4e5582f589958f131acb1ff185d
HantikMine <vadim_dr@hotmail.com>
2023-11-07 13:39:42 +0300

    -

--- app/config.py
+++ app/config.py
@@ -1,10 +1,10 @@
 import os
 SECRET_KEY = 'ajbsdiuabsidubasdiubasihdasdasdasd'
 
-MYSQL_DATABASE_USER = 'user'
-MYSQL_DATABASE_PASSWORD = 'password'
-MYSQL_DATABASE_DB = 'std_2191_exam'
-MYSQL_DATABASE_HOST = 'db'
+MYSQL_USER = 'user'
+MYSQL_PASSWORD = 'password'
+MYSQL_DB = 'std_2191_exam'
+MYSQL_HOST = 'db'
 
 ADMINISTRATOR_ROLE_ID = 1
 MODERATOR_ROLE_ID = 2
