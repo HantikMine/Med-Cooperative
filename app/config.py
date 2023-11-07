5e76a6fe732f9f5bb5041a81526fa545f61a05c3
HantikMine <vadim_dr@hotmail.com>
2023-11-07 13:41:36 +0300

    -

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ SECRET_KEY = 'ajbsdiuabsidubasdiubasihdasdasdasd'
 
 MYSQL_USER = 'user'
 MYSQL_PASSWORD = 'password'
-MYSQL_DB = 'std_2191_exam'
+MYSQL_DATABASE = 'std_2191_exam'
 MYSQL_HOST = 'db'
 
 ADMINISTRATOR_ROLE_ID = 1
