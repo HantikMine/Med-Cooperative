411d90626d5b86500a75991bfb969ac3cd7c039c
HantikMine <vadim_dr@hotmail.com>
2023-12-13 19:56:07 +0300

    first tests

---
+++ tests/config.py
@@ -0,0 +1,3 @@
+HOST_IP = "http://127.0.0.1:5000"
+LOGIN = "bgv"
+PASSWORD = "bgv"
---
+++ tests/test_main.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import time, sys
+from selenium.webdriver.common.by import By
+
+from auth import authorization
+from config import HOST_IP
+
+
+def test_main():
+   options = webdriver.ChromeOptions()
+
+   options.add_experimental_option("excludeSwitches", ["enable-logging"])
+   options.add_argument('--headless')
+   driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+
+   driver.maximize_window()
+   driver.implicitly_wait(60)
+   
+   driver.get(HOST_IP)
+
+   result_authorization = authorization(driver=driver)
+   assert result_authorization == True, "Ошибка в авторизации."
+
+   driver.close()
+   driver.quit()
