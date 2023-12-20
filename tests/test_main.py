6ddf9829a9b9c74c461f872703063654217b5ba4
HantikMine <vadim_dr@hotmail.com>
2023-12-20 17:03:38 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,6 @@
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time, sys
 from selenium.webdriver.common.by import By
